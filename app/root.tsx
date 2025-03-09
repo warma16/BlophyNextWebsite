486b57b07c6ab4a786dc3b3eee9aa82c635935d6
MojaveHao <HTTcode2020@126.com>
2025-03-09 12:29:32 +0800

    chore: 新网站图标和视频链接

--- app/root.tsx
+++ app/root.tsx
@@ -54,9 +54,7 @@ export function Layout({children}: { children: React.ReactNode }) {
         </head>
         <body className="flex flex-col min-h-screen">
         <Navbar/>
-        <main
-            className="flex-grow"
-        >
+        <main className="flex-grow">
             <div className="bg-center bg-cover  bg-fixed overflow-x-hidden" style={{
                 backgroundImage: `url(${Object.keys(currentBackground)[0]})`,
                 backgroundSize: 'cover',
